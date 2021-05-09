df92212e9fc93664c970208319eace45ffd09d46
Alexey Alekhin <laughedelic@gmail.com>
2021-05-09 17:06:42 +0200

    formatting

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import * as core from '@actions/core'
-import { execute } from 'auto'
-import parseArgs from 'auto/dist/parse-args';
+import {execute} from 'auto'
+import parseArgs from 'auto/dist/parse-args'
 
 async function run(): Promise<void> {
   try {
     const argsInput = core.getInput('args')
 
-    const [command, args] = parseArgs(argsInput.split(' '));
-    execute(command, args);
-
+    const [command, args] = parseArgs(argsInput.split(' '))
+    execute(command, args)
   } catch (error) {
     core.setFailed(error.message)
   }
