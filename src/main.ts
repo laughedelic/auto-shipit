02358fddba6870c4d674fb7add045ef0f646cdc0
Alexey Alekhin <laughedelic@gmail.com>
2021-05-10 02:12:51 +0200

    no auto

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import * as core from '@actions/core'
-import {execute} from 'auto'
-import parseArgs from 'auto/dist/parse-args'
+// import {execute} from 'auto'
+// import parseArgs from 'auto/dist/parse-args'
 
 async function run(): Promise<void> {
   try {
-    const argsInput = core.getInput('args')
-    const [command, args] = parseArgs(argsInput.split(' '))
-    await execute(command, args)
+    // const argsInput = core.getInput('args')
+    // const [command, args] = parseArgs(argsInput.split(' '))
+    // await execute(command, args)
+    console.log('foo')
   } catch (error) {
     core.setFailed(error.message)
   }
